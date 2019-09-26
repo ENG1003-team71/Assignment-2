e687133c647ad15acb0761993a0877ec27add33f
anguscostopoulos <55727925+anguscostopoulos@users.noreply.github.com>
2019-09-26 14:15:03 +1000

    Add files via upload

---
+++ route.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiYW5ndXNjb3N0b3BvdWxvcyIsImEiOiJjazBkbHljYzIwOG1rM2RzZGI3M2NudmZhIn0.fpBtta-DRI_8eveLzEGmvQ";
+let map = new  mapboxgl.Map({
+  container: 'map',
+ center: [144.9648731,-37.8182711],
+ zoom: 16,
+ style: 'mapbox://styles/mapbox/streets-v9'
+});
